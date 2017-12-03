b5d532f1a1ff60e989cccc8b4d74d35d5616d4f1
Baiyujun <baiyujun010@163.com>
2017-12-03 17:07:36 +0800

    wechat auto recrve

---
+++ flask_wechat.py
@@ -0,0 +1,70 @@
+# -*- coding:utf-8 -*-
+
+from flask import Flask, request, make_response
+import hashlib
+import xmltodict
+import time
+
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+app = Flask(__name__)
+
+
+@app.route('/wechat8000', methods=['GET', 'POST'])
+def wechat():
+    # 设置token
+    token = 'baiyujun'
+    # 获取参数
+    data = request.args
+    signature = data.get('signature')
+    timestamp = data.get('timestamp')
+    nonce = data.get('nonce')
+    echostr = data.get('echostr')
+
+    # 对参数进行排序
+    temp = [timestamp, nonce, token]
+    temp.sort()
+    temp = "".join(temp)
+    sig = hashlib.sha1(temp).hexdigest()
+    # 进行对比
+    if signature == sig:
+        # 当发来的是get请求的时候则返回echostr
+        if request.method == 'GET':
+            return echostr
+
+        # 以下的是POST请求发来的
+        xml_data = request.data
+        # 将xml转换为dict
+        xml_dict = xmltodict.parse(xml_data)['xml']
+        msg_type = xml_dict['MsgType']
+
+        if "test" == msg_type:
+            res_dict = {
+                "ToUserName": xml_dict.get("FromUserName"),
+                "FromUserName": xml_dict.get("ToUserName"),
+                "CreateTime": int(time.time()),
+                "MsgType": "text",
+                "Content": xml_dict.get("Content"),
+            }
+            print xml_dict.get("Content")
+        else:
+            res_dict = {
+                "ToUserName": xml_dict.get("FromUserName"),
+                "FromUserName": xml_dict.get("ToUserName"),
+                "CreateTime": int(time.time()),
+                "MsgType": "text",
+                "Content": "就是这么骚",
+            }
+
+        res_dict = {'xml': res_dict}
+        return xmltodict.unparse(res_dict)
+    else:
+        return 'error', 403
+
+
+if __name__ == '__main__':
+    app.run(port=8000)
+
